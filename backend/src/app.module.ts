9aaa4d751d6c62a366320b4fed57a23f735bd239
Loleo✨ <pattrapol.nope@gmail.com>
2024-10-12 21:08:58 +0700

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,23 +5,16 @@ import { BookModule } from './book/book.module';
 import { UserModule } from './user/user.module';
 import { ForumCommentModule } from './forum-comment/forum-comment.module';
 import { CommentsModule } from './comments/comments.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Module({
-  imports: [BrowserAnimationsModule,
+  imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
       port: 3306,
       database: 'book_v2',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      //ห้ามเปลี่ยนเป็น True นะไอสัส
-      //ห้ามเปลี่ยนเป็น True นะไอสัส
-      //ห้ามเปลี่ยนเป็น True นะไอสัส
-      synchronize: false, //ห้ามเปลี่ยนเป็น True
-      //ห้ามเปลี่ยนเป็น True นะไอสัส
-      //ห้ามเปลี่ยนเป็น True นะไอสัส
-      //ห้ามเปลี่ยนเป็น True นะไอสัส
+      synchronize: false, // Keep synchronize as false in production
     }),
     BookModule,
     UserModule,
